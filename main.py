008aebc6b7dfc384bb9c7c2d1775d33d673b8ef2
Paul Chung <paulchung39@gmail.com>
2016-02-24 22:41:32 -0800

    Logic v3

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-string = '"age","coun,try","location"'
+string = '"age",",coun\",try,","location"'
 
-token_begin = False
-token_end = False
-is_string = False
 
-print string + '\n'
+#is_string = False
 
-def parse(input, token_begin, token_end, is_string):
-    for i in string:
-        if i == '"':
-            return token_begin == True
-            return is_string == False
-            print '\b' + i,
-        elif i != '"':
-            return is_string == True
-            print '\b' + i,
-        elif i == ',' and token_begin == True:
-            print '\b',
+print string
+
+def parse(token):
+    token_begin = False
+    token_end = False
+
+    for i in token:
+        if token_begin == False:
+            if i == '"':
+                token_begin = True
+                token_end = False
+            elif i == ',':
+                print '\n'
+        else:
+            if i != '"':
+                print i,
+            else:
+                token_begin = False
+                token_end = True
+        #if i == '"' and token_begin == False:
+            #print '\b' + i,
+            #token_begin == True
+        #elif i != '"' and i != ',':
+            #print '\b' + i,
+            #is_string == True
+        #elif i == ',' and is_string == True:
+            #print '\b' + i,
         #elif i == '"':
             #pass
-        elif i == '"' and token_begin == True and is_string == True:
-            return token_end == True
-            print '\b' + i,
-            print '\n'
+        #elif i == '"' and token_begin == True and is_string == True:
+            #print '\b' + i,
+            #token_end == True
+        #elif i == ',' and token_end == True:
+            #print '\b' + '\n'
 
-print parse(string,False,False,False)
+print parse(string)

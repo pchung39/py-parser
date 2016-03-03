505e4ea225864b4ee118c3f23157f17769628313
Paul Chung <paulchung39@gmail.com>
2016-03-03 08:46:49 -0800

    logic v8
    all because of a comparison operator
    -___-

--- main_test.py
+++ main_test.py
@@ -1,50 +1,42 @@
-string = r'"age",ab\"c,123,",coun\",try,","location"'
-
+#string = r'"age",....ab\"c,(-1.2.3.),",c.o.u.n\",try,","location"'
+string = r'"---------",<img src=x onerror=\x32"javascript:alert(1)">'
 def parse(token):
     is_token = False
     previous_character_is_escape = False
     no_quote_value = False
     for i in token:
-        print '%s,%s' % (is_token,no_quote_value)
+        #print '%s,%s,%s' % (is_token,previous_character_is_escape,no_quote_value)
         if is_token == False:
             if i == '"':
                 print '\b' + i,
-                no_quote_value = False
                 is_token = True
-                print "1"
+                no_quote_value = False
             elif i == ',':
                 print '\n',
-                print "2"
             elif no_quote_value == True:
                 print '\b' + i,
-                print "3"
                 is_token = True
             else:
                 print '\b' + i,
-                print "4"
+
 
         elif is_token == True:
             if i == '\\':
                 print '\b' + i,
-                print "5"
                 previous_character_is_escape = True
             elif previous_character_is_escape == True and i == '"':
                 print '\b' + i,
-                print "6"
                 previous_character_is_escape = False
             elif previous_character_is_escape == False and i == '"':
                 print '\b' + i,
-                print "7"
                 is_token = False
                 no_quote_value = True
+            elif no_quote_value == True and i == ',':
+                print '\n',
+                is_token = False
             elif no_quote_value == False and i == ',':
                 print '\b' + i,
-                print "8"
-            elif no_quote_value == True and i == ',':
-                print '\n' + i,
-                print "9"
-                is_token = False
             else:
                 print '\b' + i,
-                print "10"
+
 parse(string)

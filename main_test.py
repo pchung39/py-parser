d674be9636fd959f5b34c72b23c70b33bf371047
Paul Chung <paulchung39@gmail.com>
2016-03-09 09:26:21 -0800

    Added Error Handling statement
    1. starts as quote but doesn’t have ending quote

--- main_test.py
+++ main_test.py
@@ -1,6 +1,4 @@
 import io
-#string = r'"age",....ab\"c,(-1.2.3.),",c.o.u.n\",try,","location"'
-#string = r'"---------",<img src=x onerror=\x32"javascript:alert(1)">'
 
 
 def parse(text):
@@ -8,18 +6,22 @@ def parse(text):
     is_token = False
     previous_character_is_escape = False
     no_quote_value = True
+    quote_value = False
 
+    # fancy pants row counter
     row_counter = 2
 
     with io.open(text,'rb',newline=None) as f:
-        while 1:
+        while True:
             byte = f.read(1)
             for i in byte:
+                print "%s,%s" % (quote_value,no_quote_value)
                 if is_token == False:
                     if i == '"':
                         print '\b' + i,
                         is_token = True
                         no_quote_value = False
+                        quote_value = True
                     elif i == '\n':
                         print '\n'
                     elif i == ',':
@@ -27,11 +29,13 @@ def parse(text):
                     elif no_quote_value == True:
                         print '\b' + i,
                         is_token = True
+                        quote_value = False
                     else:
                         print '\b' + i,
 
 
                 elif is_token == True:
+                    # start of an escape sequence
                     if i == '\\':
                         print '\b' + i,
                         previous_character_is_escape = True
@@ -41,20 +45,32 @@ def parse(text):
                         print "This is row %i" % (row_counter)
                         row_counter += 1
                         is_token = False
-                    elif i == '\{}'.format()
+                    # builds off previous_character_is_escape and captures any escape sequence
                     elif previous_character_is_escape == True:
                         print '\b' + i,
                         previous_character_is_escape = False
+                    # this type of quote is the end of token, returns back to other if statement
                     elif previous_character_is_escape == False and i == '"':
                         print '\b' + i,
-                        is_token = False
                         no_quote_value = True
+                        quote_value = False
+                        is_token = False
+                    # rework this state, this is the same as two loops below
+                    elif no_quote_value == True and previous_character_is_escape == False and i == ',':
+                        print '\n' + "This is not a valid token"
+                        return False
+                    # if token starts as a quote but ends without quotes
+                    elif quote_value == True and previous_character_is_escape == False and i == ',':
+                        print '\n' + "This is not a valid token"
+                        return False
+                    # differentiating between commas that should be considered part of the string and those that should not be
                     elif no_quote_value == True and i == ',':
                         print '\n' + '\b',
+                        no_quote_value = False
                         is_token = False
                     elif no_quote_value == False and i == ',':
                         print '\b' + i,
                     else:
                         print '\b' + i,
 
-parse('SampleCSVFile.csv')
+parse('example.csv')

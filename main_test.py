9f5ce1d910f54140e583c4fc5154a043732446fb
Paul Chung <paulchung39@gmail.com>
2016-03-27 20:52:39 -0700

    Added logic for token counter and writing to file

--- main_test.py
+++ main_test.py
@@ -1,4 +1,13 @@
 import io
+import os
+from os import path
+from os.path import basename
+
+
+
+#file_base = os.path.basename('"app/csv_upload_directory/%s.csv' % filename)
+#new_base = os.path.splitext(file_base)[0]
+#file_path = path.relpath("app/csv_upload_directory/%s_result.csv" % new_base)
 
 
 def parse(text):
@@ -8,14 +17,117 @@ def parse(text):
     no_quote_value = True
     quote_value = False
 
-    # fancy pants row counter
-    row_counter = 2
 
+    file_base = os.path.basename('"app/csv_upload_directory/%s' % text)
+    new_base = os.path.splitext(file_base)[0]
+    #file_path = path.relpath("app/csv_upload_directory/%s_result.csv" % new_base)
+
+
+
+    row_counter = 1
+    token_counter = 0
+    fo = open("csv_upload_directory/%s_results.csv" % new_base, "w+")
+
+    fo.write("Row %i" % row_counter + '\n')
+    row_counter += 1
     with io.open(text,'rb',newline=None) as f:
         while True:
             byte = f.read(1)
             for i in byte:
-                #print "%s,%s" % (quote_value,no_quote_value)
+                #print "%s,%s" % (no_quote_value,previous_character_is_escape)
+                if is_token == False:
+                    if i == '"':
+                        fo.write(i)
+                        token_counter = 0
+                        is_token = True
+                        no_quote_value = False
+                        quote_value = True
+                    elif i == '\n':
+                        fo.write(" ")
+                        fo.write("(%i)" % token_counter)
+                        fo.write('\n')
+                        fo.write("Row %i" % (row_counter))
+                        fo.write("\n")
+                        token_counter = 0
+                        row_counter += 1
+                    elif i == ',':
+                        fo.write(" ")
+                        fo.write("(%i)" % token_counter)
+                        fo.write('\n')
+                        token_counter = 0
+                    elif no_quote_value == True:
+                        fo.write(i)
+                        token_counter += 1
+                        is_token = True
+                        quote_value = False
+                    else:
+                        fo.write(i)
+                        token_counter += 1
+
+
+                elif is_token == True:
+                    # start of an escape sequence
+                    if i == '\\':
+                        fo.write(i)
+                        token_counter += 1
+                        previous_character_is_escape = True
+                    # for line delimiter, the quoted values are being processed outside token
+                    elif no_quote_value == True and i == '\n':
+                        fo.write(" ")
+                        fo.write("(%i)" % token_counter)
+                        fo.write('\n')
+                        fo.write("Row %i" % (row_counter))
+                        fo.write("\n")
+                        token_counter = 0
+                        row_counter += 1
+                        is_token = False
+                    # if token is not a quoted value but ends with quotes, and there is no escape character
+                    elif no_quote_value == True and previous_character_is_escape == False and i == '"':
+                        fo.write(i)
+                        fo.write("This is a not a valid token, this is not a quoted value but there is an ending quote")
+                        return False
+                    # builds off previous_character_is_escape and captures any escape sequence
+                    elif previous_character_is_escape == True:
+                        fo.write(i)
+                        token_counter += 1
+                        previous_character_is_escape = False
+                    # this type of quote is the end of token, returns back to other if statement
+                    elif previous_character_is_escape == False and i == '"':
+                        fo.write(i)
+                        no_quote_value = True
+                        quote_value = False
+                        is_token = False
+                    # if token starts as a quote but ends without quotes
+                    elif quote_value == True and previous_character_is_escape == False and i == ',':
+                        fo.write(i)
+                        fo.write("This is not a valid token, there should be a quote at the end of this token")
+                        return False
+                    # this comma marks the end of a non quoted token, this invokes a newline
+                    elif no_quote_value == True and previous_character_is_escape == False and i == ',':
+                        fo.write(" ")
+                        fo.write("(%i)" % token_counter)
+                        fo.write('\n')
+                        token_counter = 0
+                        is_token = False
+                    elif no_quote_value == False and i == ',':
+                        fo.write(i)
+                        fo.write("DONG")
+                    else:
+                        fo.write(i)
+                        token_counter += 1
+        fo.close()
+        f.close()
+
+parse('example.csv')
+
+'''
+    print "This is row %i" % (row_counter)
+    row_counter += 1
+    with io.open(file_path,'rb',newline=None) as f:
+        while True:
+            byte = f.read(1)
+            for i in byte:
+                #print "%s,%s" % (no_quote_value,previous_character_is_escape)
                 if is_token == False:
                     if i == '"':
                         print '\b' + i,
@@ -24,6 +136,8 @@ def parse(text):
                         quote_value = True
                     elif i == '\n':
                         print '\n'
+                        print "This is row %i" % (row_counter)
+                        row_counter += 1
                     elif i == ',':
                         print '\n' + '\b',
                     elif no_quote_value == True:
@@ -39,15 +153,16 @@ def parse(text):
                     if i == '\\':
                         print '\b' + i,
                         previous_character_is_escape = True
-                    # for line delimiter
-                    elif i == '\n':
+                    # for line delimiter, the quoted values are being processed outside token
+                    elif no_quote_value == True and i == '\n':
                         print '\n'
                         print "This is row %i" % (row_counter)
                         row_counter += 1
                         is_token = False
                     # if token is not a quoted value but ends with quotes, and there is no escape character
                     elif no_quote_value == True and previous_character_is_escape == False and i == '"':
-                        print '\n' + "This is a not a valid token, this is not a quoted value but there is an ending quote"
+                        print '\b' + i,
+                        print "(This is a not a valid token, this is not a quoted value but there is an ending quote)"
                         return False
                     # builds off previous_character_is_escape and captures any escape sequence
                     elif previous_character_is_escape == True:
@@ -61,22 +176,16 @@ def parse(text):
                         is_token = False
                     # if token starts as a quote but ends without quotes
                     elif quote_value == True and previous_character_is_escape == False and i == ',':
-                        print '\n' + "This is not a valid token, there should be a quote at the end of this token"
+                        print '\b' + i,
+                        print "(This is not a valid token, there should be a quote at the end of this token)"
                         return False
                     # this comma marks the end of a non quoted token, this invokes a newline
                     elif no_quote_value == True and previous_character_is_escape == False and i == ',':
                         print '\n' + '\b',
-                        no_quote_value = False
                         is_token = False
                     elif no_quote_value == False and i == ',':
                         print '\b' + i,
                     else:
                         print '\b' + i,
 
-parse('example.csv')
-
-'''
-def quote_val(byte):
-
-def non_quote_val(byte):
 '''
